480b2bfc98a224fea2617421090c258173eb8192
petetetete <huettlpeter@gmail.com>
2018-02-15 19:33:22 -0700

    Move load graph function to GraphSearch

--- __main__.py
+++ __main__.py
@@ -7,27 +7,6 @@ from search import GraphSearch
 
 import sys
 
-
-# def load_graph(file_name):
-
-#   # Get file contents
-#   with open(file_name) as file:
-#     lines = [line.rstrip() for line in file]
-
-#   # Iterate over file lines
-#   for line in lines:
-
-#     # Remove unecessary characters
-#     line = line.replace('\'', '')\
-#                .replace('[', '').replace(']', '').replace(' ', '').strip('()')
-
-#     # Unpack line into variables
-#     [n1, n2, ev, x1, y1, x2, y2] = line.split(",")
-#     print(ev)
-
-
-# load_graph("data/30.txt")
-
 # g = GraphViz()
 # g.loadGraphFromFile("test_data/30.txt")
 # g.plot()
--- search/GraphSearch.py
+++ search/GraphSearch.py
@@ -7,10 +7,29 @@ from graph import GraphViz
 
 class GraphSearch:
 
-  def __init__(self):
+  def __init__(self, graph_file):
     self.test = "ASDF"
+
+    if graph_file is not None:
+      self.load_graph(graph_file)
+
+  def load_graph(self, file_name):
+
+    # Get file contents
+    with open(file_name) as file:
+      lines = [line.rstrip() for line in file]
+
+    # Iterate over file lines
+    for line in lines:
+
+      # Remove unecessary characters
+      line = line.replace('\'', '').replace('[', '')\
+                 .replace(']', '').replace(' ', '').strip('()')
+
+      # Unpack line into variables
+      [n1, n2, ev, x1, y1, x2, y2] = line.split(",")
 
 
 # Test the class
 if __name__ == "main":
-  print("Tests here")
+  print("[TODO] Tests here...")
